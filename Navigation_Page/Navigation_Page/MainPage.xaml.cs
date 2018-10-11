8b5bd79dc819a6ea2d782fd6b3add55861ba61e4
G00365218 <G00365218@CRBYOD17>
2018-10-11 14:33:00 +0100

    Add project files.

---
+++ Navigation_Page/Navigation_Page/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Navigation_Page
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            bool isUserEmpty = string.IsNullOrEmpty(entryUsername.Text);
+            bool isPassEmpty = string.IsNullOrEmpty(entryPassword.Text);
+
+            if( isUserEmpty || isPassEmpty)
+            {
+
+            }
+            else
+            {
+                //All conditions met to navigate forward
+            }
+        }
+
+        private void btnClicked_Tool(object sender, EventArgs)
+        {
+
+        }
+
+	}
+}
